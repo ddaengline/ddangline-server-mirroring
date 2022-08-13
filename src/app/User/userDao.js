aaf286b9b3d685bf563141a8cce92cff88ae88f1
Ucan <zyzy1231@naver.com>
2022-08-14 02:23:35 +0900

    feat: add delete user

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -1,4 +1,6 @@
 const { User } = require('../../models/User');
+const { Collection } = require('../../models/Collection')
+const { Place } = require('../../models/Place')
 
 // 유저 생성
 async function createUser(insertUserInfoParams){
@@ -11,12 +13,12 @@ async function getUsers(){
   return User.find({}, { _id: 1, username: 1, email: 1 });
 }
 
-async function getUser(id){
-  return User.findById(id, { _id: 1, username: 1, email: 1 })
+async function getUser(_id){
+  return User.findOne({ _id, status: "ACTIVE" }, { _id: 1, username: 1, email: 1 })
 }
 
-async function updateUserName(id, name){
-  return User.findOneAndUpdate({ _id: id }, { username: name }, {
+async function updateUserName(_id, name){
+  return User.findOneAndUpdate({ _id, status: "ACTIVE" }, { username: name }, {
     runValidators: true,
     new: true,
     projection: { _id: 1, username: 1 }
@@ -24,7 +26,7 @@ async function updateUserName(id, name){
 }
 
 async function updateUserPassword(id, pw){
-  return User.findOneAndUpdate({ _id: id }, { password: pw }, {
+  return User.findOneAndUpdate({ _id: id, status: "ACTIVE" }, { password: pw }, {
     runValidators: true,
     new: true,
     projection: { _id: 1 }
@@ -32,11 +34,19 @@ async function updateUserPassword(id, pw){
 }
 
 async function deleteUser(id){
-  return User.findByIdAndDelete({ _id: id }, { status: 'WITHDRAWAL' }, {
+  // TODO: 유저 상태 변경'WITHDRAWAL', 유저 수납장 삭제, 가게 좋아요/싫어요/갔다옴 pull
+  const[deletedUser] = await Promise.all([User.findOneAndUpdate({ _id: id }, { status: 'WITHDRAWAL' }, {
     runValidators: true,
     new: true,
     projection: { _id: 1 }
-  })
+  }),
+    Collection.deleteMany({ userId: id }), Place.updateMany({},
+      {
+        $pull: { "liked.$[user]": id, "marked.$[user]": id, "visited.$[user]": id },
+        $inc: { liked: -1, marked: -1, visited: -1 }
+      },
+      { arrayFilters: [{ "user": id }] })])
+  return deletedUser
 }
 
 async function getUserByEmail(email){
