39becdb21b70dc3cd2ba2090b79c6603b5e01b5d
Ucan <zyzy1231@naver.com>
2022-08-20 16:48:29 +0900

    fix: modify email duplication

--- src/app/Mail/mailRouter.js
+++ src/app/Mail/mailRouter.js
@@ -3,10 +3,8 @@ module.exports = function (app) {
 
   // 인증 코드 전송
   app.post('/app/v1/email/code', mail.sendCode);
-
   // 인증 코드 점검
   app.post('/app/v1/email/code/auth', mail.verifyCode);
-
   // 이메일 중복
   app.post('/app/v1/email', mail.checkEmailDuplication);
 };
--- src/app/Mail/mailService.js
+++ src/app/Mail/mailService.js
@@ -7,25 +7,27 @@ const baseResponse = require('../../../config/baseResponseStatus');
 const nodemail = require('nodemailer');
 const ejs = require('ejs');
 const path = require('path');
+const { logger } = require("../../../config/winston");
 var appDir = path.dirname(require.main.filename);
 require('dotenv').config();
 
 // 이메일 중복 검사
-exports.checkEmailDuplication = async (email) => {
+exports.checkEmailDuplication = async(email) => {
   try {
     const connection = await mongoose.connect(MONGO_URI, { dbName });
-    const emailUser = await User.findOne({ email });
+    const emailUser = await User.findOne({ email, status: "ACTIVE" });
     if (emailUser) return errResponse(baseResponse.SIGNUP_REDUNDANT_EMAIL);
     connection.disconnect();
     const { isSuccess, code } = baseResponse.SUCCESS;
     return response({ isSuccess, code, message: '중복된 이메일이 없습니다.' });
-  } catch (error) {
+  } catch(err) {
+    logger.error(`App - checkEmailDuplication Service error\n: ${err.message}`)
     return errResponse(baseResponse.DB_ERROR);
   }
 };
 
 // 인증 코드 전송
-exports.sendCode = async (mailToSend) => {
+exports.sendCode = async(mailToSend) => {
   try {
     let authCode = Math.random().toString(10).substr(2, 4);
     const params = { email: mailToSend, code: authCode };
@@ -52,7 +54,6 @@ exports.sendCode = async (mailToSend) => {
       text: '땡호선 인증번호 전송', // plain text body
       html: emailTemplate,
     };
-
     const connection = await mongoose.connect(MONGO_URI, { dbName });
     let mailObject = await MailAuthentication.findOneAndDelete({ email: mailToSend });
     mailObject = new MailAuthentication(params);
@@ -61,24 +62,24 @@ exports.sendCode = async (mailToSend) => {
     Promise.all([connection.disconnect(), transporter.close()]);
     const { isSuccess, code } = baseResponse.SUCCESS;
     return response({ isSuccess, code, message: '인증 코드 전송 성공' });
-  } catch (err) {
-    console.log({ err });
+  } catch(err) {
+    logger.error(`App - sendCode Service error\n: ${err.message}`)
     return errResponse(baseResponse.DB_ERROR);
   }
 };
 
 // 인증 코드 validation
-exports.verifyCode = async (email, validationCode) => {
+exports.verifyCode = async(email, validationCode) => {
   try {
     const connection = await mongoose.connect(MONGO_URI, { dbName });
     const mailObject = await MailAuthentication.findOne({ email });
-    if (!mailObject) return errResponse(baseResponse.AUTH_MAIL_WRONG); // 3분 만료 후에도.
+    if (!mailObject) return errResponse(baseResponse.AUTH_MAIL_WRONG); // 3분 만료 됨.
     else if (mailObject.code !== validationCode) return errResponse(baseResponse.AUTH_CODE_WRONG);
     connection.disconnect();
     const { isSuccess, code } = baseResponse.SUCCESS;
     return response({ isSuccess, code, message: '이메일 인증 성공' });
-  } catch (err) {
-    console.log({ err });
+  } catch(err) {
+    logger.error(`App - verifyCode Service error\n: ${err.message}`)
     return errResponse(baseResponse.DB_ERROR);
   }
 };
